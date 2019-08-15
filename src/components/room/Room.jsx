2dfe2ac1b4271ff4b93bb2cfec41d94bf3410ecf
Alvaro Marquez <marquezalvaro13@gmail.com>
2019-08-15 15:45:50 +0200

    Add room component

---
+++ src/components/room/Room.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './room.scss';
+
+export const Room = ({img, title, price, currency}) => (
+  <div className="room-card">
+    <div className="img-container">
+      <img src={img} alt="room photo"></img>
+    </div>
+    <div className="room-info">
+      <div className="room-title">
+        <span className="title">{title}</span>
+        <span className="price-container">
+          <span className="price">{price}{currency}</span>
+        </span>
+      </div>
+      <div className="buttons">
+        <button className="secondary">More details</button>
+        <button className="primary">Book now</button>
+      </div>
+    </div>
+  </div>
+) 
