895f46cb6b95f6df041ebe10730fe3a4d3e90ed4
Alvaro Marquez <marquezalvaro13@gmail.com>
2019-08-15 12:50:32 +0200

    Add index files and API

--- src/api/index.js
+++ src/api/index.js
@@ -3,21 +3,36 @@ var app = express();
 const https = require('https');
 
 
-app.get('/api/homecards', (req, res) => {
-  let buffers = []
+app.get('/api/homecards', (appReq, appRes) => {
+  let idsRequestBuffers = []
   https.get('https://frontend-interview.spotahome.com/api/public/listings/search/markers/madrid', 
-    response => {
-      response.on('error', err => console.error(err))
-      response.on('data', (data) => {
-        buffers.push(data)
-      });
-      response.on('end', data => {
-        if(response.statusCode === 200) {
-          const buffer = Buffer.concat(buffers);
-          const data = JSON.parse(buffer.toString()).data.slice(0, 30)
-          res.send(data)
+    idsResponse => {
+      idsResponse.on('error', err => console.error(err))
+      idsResponse.on('data', (data) => { idsRequestBuffers.push(data) });
+      idsResponse.on('end', data => {
+        if(idsResponse.statusCode === 200) {
+          const buffer = Buffer.concat(idsRequestBuffers);
+          const ids = JSON.parse(buffer.toString()).data.slice(0, 31)
+            .map(marker => marker.id);
+          const idsQuery = `?ids[]= ${ids.join('&ids[]=')}`
+          https.get(`https://frontend-interview.spotahome.com/api/public/listings/search/homecards_ids?${idsQuery}`,
+            homeCardsResponse => {
+              let homeCardsBuffers = [];
+              homeCardsResponse.on('error', err => console.error(err))
+              homeCardsResponse.on('data', data => {
+                homeCardsBuffers.push(data);
+              });
+              homeCardsResponse.on('end', () => {
+                if(homeCardsResponse.statusCode === 200) {
+                  const homeCardsBuffer = Buffer.concat(buff);
+                  const homecards = JSON.parse(homeCardsBuffer.toString())
+                    .data.homecards
+                  res.send(homecards)
+                }
+              })
+            })
         } else {
-          console.error('The request failed')
+          console.error(`The request failed with the following code ${resp.statusCode}`)
         }
       });
     })
